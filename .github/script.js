2dab90438582d7aef3efdb9584a791ff5227d039
Elizabeth Mites <elizabethmites2703@gmail.com>
2025-05-26 20:13:49 -0500

    Funcion para motrar paises- Modificación en script

--- .github/script.js
+++ .github/script.js
@@ -13,3 +13,17 @@ fetch('https://restcountries.com/v3.1/all')
         divResultado.innerHTML = '<p>Error al cargar los países</p>';
         console.error(error);
     });
+    
+function mostrarPaises(paises) {
+    divResultado.innerHTML = '';
+    paises.forEach(pais => {
+        const div = document.createElement('div');
+        div.classList.add('pais');
+        div.innerHTML = `
+      <h3>${pais.name.official}</h3>
+      <img src="${pais.flags.svg}" alt="Bandera de ${pais.name.common}">
+      <p><strong>Región:</strong> ${pais.region}</p>
+      <p><strong>Población:</strong> ${pais.population.toLocaleString()}</p>`;
+        divResultado.appendChild(div);
+    });
+}
